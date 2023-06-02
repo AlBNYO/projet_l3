62761b81cc5d4a438f8e4d0077142a6b231f18b2
AlBNYO <135336149+AlBNYO@users.noreply.github.com>
2023-06-02 14:17:54 +0200

    Add files via upload

---
+++ streamlittest1.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+import numpy as np
+import streamlit as st
+from textblob import TextBlob
+from transformers import AutoTokenizer, TFAutoModelForSequenceClassification
+from transformers import pipeline
+
+tokenizer = AutoTokenizer.from_pretrained("tblard/tf-allocine")
+model = TFAutoModelForSequenceClassification.from_pretrained("tblard/tf-allocine")
+
+nlp = pipeline('sentiment-analysis', model=model, tokenizer=tokenizer)
+
+def analyze_sentiment(sentiment):
+    result=nlp(sentiment)[0]
+    return result
+def main():
+    st.title("Analyse de sentiment")
+    st.write("Entrez un sentiment pour l'analyser :")
+    sentiment = st.text_input("Sentiment")
+
+    if st.button("Analyser"):
+        result = analyze_sentiment(sentiment)
+        polarity = np.array(result['label'])
+        score=np.array(result['score'])
+        st.write("Le sentiment est :", np.array2string(polarity), " à ",np.array2string(score)," de certitude")
+
+if __name__ == "__main__":
+    main()
+
+
+# In[ ]:
