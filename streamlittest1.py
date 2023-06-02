d6dc74dfc626a778dc75cdaa81356cd53bce5165
AlBNYO <135336149+AlBNYO@users.noreply.github.com>
2023-06-02 14:56:32 +0200

    Add files via upload

--- streamlittest1.py
+++ streamlittest1.py
@@ -8,7 +8,7 @@ import streamlit as st
 from transformers import AutoTokenizer, TFAutoModelForSequenceClassification
 from transformers import pipeline
 
-tokenizer = AutoTokenizer.from_pretrained("tblard/tf-allocine")
+tokenizer = AutoTokenizer.from_pretrained("tblard/tf-allocine",use_fast= False)
 model = TFAutoModelForSequenceClassification.from_pretrained("tblard/tf-allocine")
 
 nlp = pipeline('sentiment-analysis', model=model, tokenizer=tokenizer)
